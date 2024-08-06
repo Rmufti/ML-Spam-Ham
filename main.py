b910e2a96115a98096537b7ff9351821b065b79e
Rmufti <121902884+Rmufti@users.noreply.github.com>
2024-08-06 15:31:04 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+#Imports
+import string
+
+import numpy as np
+import pandas as pd
+
+import nltk
+nltk.download()
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+
+from sklearn.ensemble import RandomForestClassifier
+
+
+#Checklist
+#nltk.download()
+
+df = pd.read_csv('spam_ham_dataset.csv')
+
+df['text'] = df['text'].apply(lambda x: x.replace('\r\n', ' '))
+
+
+#Stemmer
+
+stemmer = PorterStemmer()
+corpus = []
+
+
+stopwords_set = set(stopwords.words('english'))
+
+for i in range(len(df)):
+  text = df['text'].iloc[i].lower()
+  text = text.translate(str.maketrans(' ', ' ', string.punctuation)).split()
+  text = [stemmer.stem(word) for word in text if word not in stopwords_set]
+  text = ' '.join(text)
+  corpus.append(text)
+
+#Vectorization
+
+vectorizer = CountVectorizer()
+
+x = vectorizer.fit_transform(corpus).toarray()
+
+y = df.label_num
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
+
+clf = RandomForestClassifier(n_jobs = -1)
+
+clf.fit(x_train, y_train)
+
+clf.score(x_test, y_test)
